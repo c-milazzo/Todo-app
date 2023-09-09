33f208767c9f86f2fb960ecfdfccc8878f31e0df
Chris Milazzo <Christophermilazzo@hotmail.com>
2023-09-09 14:54:58 -0400

    Implement: remove checked task, include read/me

--- cli.py
+++ cli.py
@@ -7,7 +7,7 @@ print("It is", now)
 
 
 while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = input("Type add, show, edit, complete or exit:")
     user_action = user_action.strip()
 
     if user_action.startswith("add"):
